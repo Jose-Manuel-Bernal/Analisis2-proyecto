5122d438d0a00734d209d0e7d6b6e5420b627c3d
Jose Manuel Bernal Aguilar <josem.bernal@udea.edu.co>
2022-07-22 21:14:49 -0500

    proyecto base creado

---
+++ src/main/java/com/udea/analisis2/proyecto/bibliotecadeConocimientos/BibliotecaDeConocimientosApplication.java
@@ -0,0 +1,13 @@
+package com.udea.analisis2.proyecto.bibliotecadeConocimientos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaDeConocimientosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaDeConocimientosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udea/analisis2/proyecto/bibliotecadeConocimientos/BibliotecaDeConocimientosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udea.analisis2.proyecto.bibliotecadeConocimientos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaDeConocimientosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
